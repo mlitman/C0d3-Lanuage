e126c36c4e591345e6b0c707eb40235784f49be3
mlitman <awesomefat@gmail.com>
2020-11-19 13:58:30 -0600

    Update objectOrientedInterpreter.rkt

--- objectOrientedInterpreter.rkt
+++ objectOrientedInterpreter.rkt
@@ -1,22 +1,25 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname objectOrientedInterpreter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+;global-env is a list of scopes where a scope is a list of name/value pairs (2-lists)
+
 ;constructors
-(define empty-object
-  (lambda ()
-    (list 'empty-object)))
 
 (define class
-  (lambda (base-class list-of-fields list-of-values list-of-methods)
+  (lambda (base-class list-of-field-names list-of-field-values list-of-method-names list-of-method-values)
     (letrec ((create-pairs-for-scope (lambda (lon lov)
                              (cond
                                ((null? lon) '())
                                (else (cons (list (car lon)
                                                  (car lov))
                                            (create-pairs-for-scope (cdr lon) (cdr lov)))))))
-             (global-env (list (create-pairs-for-scope list-of-fields
-                                                       list-of-values))))
+             (global-env (list (create-pairs-for-scope list-of-field-names
+                                                       list-of-field-value))))
       (list 'class base-class global-env list-of-methods))))
+
+(define empty-object
+  (lambda ()
+    (class '(no-parent) '() '() '(getValue) '((lambda (var-name) 'need-logic-here)))))
 
 
 
@@ -26,7 +29,7 @@
 
 (define p1 (Person '(fname lname age) '(Mike Litman 21)))
 (define p2 (Person '(fname lname age) '(Dave Smith 18)))
-p1
+;(send-message p1 getField '(fname))
 p2
 
 
